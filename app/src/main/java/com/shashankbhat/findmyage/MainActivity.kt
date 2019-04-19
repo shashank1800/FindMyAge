bcf70efc1e68b4300ebfd3545ddce13193b3a37e
SHASHANK BHAT <shashankbhat1800@gmail.com>
2019-04-19 23:43:01 +0530

    bug fix

--- app/src/main/java/com/shashankbhat/findmyage/MainActivity.kt
+++ app/src/main/java/com/shashankbhat/findmyage/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shashankbhat.findmyage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val context = this
+
         findIt.setOnClickListener {
             val year = year.text.toString().toInt()
             val currentYear = Calendar.getInstance().get(Calendar.YEAR)
@@ -18,7 +21,7 @@ class MainActivity : AppCompatActivity() {
                 val age = currentYear - year
                 printAge.text = "Your age is "+age
             }else{
-
+                Toast.makeText(context,"Age cannot be more than Current Year!",Toast.LENGTH_SHORT).show()
             }
         }
     }
