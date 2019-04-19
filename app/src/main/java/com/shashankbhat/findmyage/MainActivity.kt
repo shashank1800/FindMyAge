af9b363893345cbb6197996783a8fb8c9ec8ecc1
SHASHANK BHAT <shashankbhat1800@gmail.com>
2019-04-19 23:29:49 +0530

    Initial commit

---
+++ app/src/main/java/com/shashankbhat/findmyage/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.shashankbhat.findmyage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findIt.setOnClickListener {
+            val year = year.text.toString().toInt()
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+            if(year<currentYear){
+                val age = currentYear - year
+                printAge.text = "Your age is "+age
+            }else{
+
+            }
+        }
+    }
+}
